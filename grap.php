65964761c41961d449e478c0864c308689919a15
Alex Samorukov <samm@os2.kiev.ua>
2016-09-06 13:13:24 +0200

    update data, disable json

--- grap.php
+++ grap.php
@@ -17,8 +17,8 @@ $fp=fopen('decl/index.md','w');
 foreach($matches[1] as $key=>$v){
     fwrite($fp,"- [".trim($matches[2][$key])."](https://cdn.rawgit.com/samm-git/nazk-data/master/decl/".$v."/decl.html)\n");
     if(!is_dir('decl/'.$v)) mkdir('decl/'.$v);
-    $content=file_get_contents('https://public-api.nazk.gov.ua/declarations-public/api/declaration/'.$v);
-    if($content)  file_put_contents("decl/".$v."/decl.json", $content);
+//    $content=file_get_contents('https://public-api.nazk.gov.ua/declarations-public/api/declaration/'.$v);
+//    if($content)  file_put_contents("decl/".$v."/decl.json", $content);
     $content=file_get_contents('https://public.nazk.gov.ua/declaration/'.$v);
     if($content) file_put_contents("decl/".$v."/decl.html", $content);
 
